73cef25061d0eb36ed9d70f3ae2b39ed1f275050
danielkliu07-beep <danielkliu07@gmail.com>
2026-06-28 19:10:23 -0400

    Added function names

---
+++ translator.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+
+#English -> Chinese
+
+class PositionEncoding(nn.Module):
+    pass
+
+class Attention(nn.Module):
+    pass
+
+class EncoderRnn(nn.Module):
+
+    pass
+
+class DecoderRnn(nn.Module):
+
+    pass
