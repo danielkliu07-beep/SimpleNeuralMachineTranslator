f884c4a1f95a25413f66a8f4b8b39d175f542669
danielkliu07-beep <danielkliu07@gmail.com>
2026-06-28 19:32:11 -0400

    Added functions

--- translator.py
+++ translator.py
@@ -1,17 +1,73 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+
+
+from torch.utils.data import TensorDataset, DataLoader
 
 #English -> Chinese
 
 class PositionEncoding(nn.Module):
-    pass
+
+    def __init__(self, d_model = 512, max_len = 1000): 
+
+        super().__init__()
+
+        pe = torch.zeros(max_len, d_model) 
+
+        position = torch.arange(start = 0, end = max_len, step = 1).float().unsqueeze(1) 
+        embedding_index = torch.arange(start = 0, end = d_model, step = 2).float() 
+
+        div_term = 1 / torch.tensor(10000.0)**(embedding_index / d_model)
+
+        pe[:, 0::2] = torch.sin(position * div_term) 
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        self.register_buffer('pe', pe) 
+    
+    def forward(self, word_embeddings):
+
+        return word_embeddings + self.pe[:word_embeddings.size(0), :] 
+
 
 class Attention(nn.Module):
-    pass
+
+    def __init__(self, d_model = 2):
+
+        super().__init__()
+
+        self.W_q = nn.Linear(in_features = d_model, out_features = d_model, bias = False) 
+        self.W_k = nn.Linear(in_features = d_model, out_features = d_model, bias = False) 
+        self.W_v = nn.Linear(in_features = d_model, out_features = d_model, bias = False) 
+
+        self.row_dim = 0
+        self.col_dim = 1
+
+    def forward(self, encodings_for_q, encodings_for_k, encodings_for_v, mask=None):
+
+        q = self.W_q(encodings_for_q) 
+        k = self.W_k(encodings_for_k)
+        v = self.W_v(encodings_for_v)
+
+        sims = torch.matmul(q, k.transpose(dim0=self.row_dim, dim1 = self.col_dim))
+        scaled_sims = sims / (k.size(self.col_dim) ** 0.5)
+
+        if mask is not None: 
+            scaled_sims = scaled_sims.masked_fill(mask = mask, value = -1e9) 
+
+        attention_percents = F.softmax(scaled_sims, dim = self.col_dim) 
+
+        attention_scores = torch.matmul(attention_percents, v) 
+
+        return attention_scores
 
 class EncoderRnn(nn.Module):
 
-    pass
+    def __init__(self, num_tokens, d_model, max_len):
+
+        super().__init__()
+
+        self.lstm = nn.LSTM(input_size = 1, hidden_size = 1, num_layers = 8)
 
 class DecoderRnn(nn.Module):
 
