00c9ddc6607e6386839f67adf47edca9265b0613
danielkliu07-beep <danielkliu07@gmail.com>
2026-06-29 11:52:18 -0400

    Added changes

--- translator.py
+++ translator.py
@@ -45,6 +45,8 @@ class EncoderRnn(nn.Module):
 
         super().__init__()
 
+        self.we = nn.Embedding(num_embeddings = num_tokens, embedding_dim = d_model)
+
         self.bi_lstm = nn.LSTM(input_size = 1, hidden_size = 1, num_layers = 1, bidirectional=True)
         
         self.uni_lstm1 = nn.LSTM(input_size = 1, hidden_size = 1, num_layers = 1)
@@ -55,10 +57,38 @@ class EncoderRnn(nn.Module):
         self.uni_lstm6 = nn.LSTM(input_size = 1, hidden_size = 1, num_layers = 1)
         self.uni_lstm7 = nn.LSTM(input_size = 1, hidden_size = 1, num_layers = 1)
 
-    def forward(self, input):
+    def forward(self, token_id):
 
-        bi_lstm_out, temp1 = self.bi_lstm(input)
-        uni_lstm_out, temp2 = self.uni_lstm(bi_lstm_out)
+        word_embedding1 = self.we(token_id)
+        bi_lstm_out = self.bi_lstm(word_embedding1)
+
+        uni_lstm_out1 = self.uni_lstm1(bi_lstm_out)
+    
+        uni_lstm_out2 = self.uni_lstm2(uni_lstm_out1)
+        residual_connection_value1 = uni_lstm_out2 + uni_lstm_out1
+
+        uni_lstm_out3 = self.uni_lstm3(residual_connection_value1)
+        residual_connection_value2 = residual_connection_value1 + uni_lstm_out3
+
+        uni_lstm_out4 = self.uni_lstm4(residual_connection_value2)
+        residual_connection_value3 = residual_connection_value2 + uni_lstm_out4
+
+        uni_lstm_out5 = self.uni_lstm5(residual_connection_value3)
+        residual_connection_value4 = residual_connection_value3 + uni_lstm_out5
+        
+
+        uni_lstm_out6 = self.uni_lstm6(residual_connection_value4)
+        residual_connection_value5 = residual_connection_value4 + uni_lstm_out6
+        
+
+        uni_lstm_out7, temp = self.uni_lstm7(residual_connection_value5)
+        residual_connection_value6 = residual_connection_value5 + uni_lstm_out7
+
+        return residual_connection_value6
+        
+
+
+
 
 class DecoderRnn(nn.Module):
 
