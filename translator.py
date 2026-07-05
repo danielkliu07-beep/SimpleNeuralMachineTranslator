e73bcca93c8ed10736d13fabb1798001832af251
danielkliu07-beep <danielkliu07@gmail.com>
2026-07-05 17:22:15 -0400

    Finished first draft of multiheadattention class

--- translator.py
+++ translator.py
@@ -16,7 +16,7 @@ class Head(nn.Module):
 
     #Cross attention
 
-    def __init__(self, embed_dim, dropout):
+    def __init__(self, embed_dim, head_size, dropout):
         super().__init__()
 
         #query (query_dim x embed_dim)
@@ -24,10 +24,10 @@ class Head(nn.Module):
         #value_up (query_dim x embed_dim)
         #value_down (embed_dim * query_dim)
 
-        self.query = nn.Linear(embed_dim, embed_dim)
+        self.query = nn.Linear(embed_dim, head_size)
 
-        self.key = nn.Linear(embed_dim, embed_dim)
-        self.value = nn.Linear(embed_dim, embed_dim)
+        self.key = nn.Linear(embed_dim, head_size)
+        self.value = nn.Linear(embed_dim, head_size)
 
         self.dropout = nn.Dropout(dropout)
     
@@ -50,22 +50,34 @@ class Head(nn.Module):
 
 class MultiHeadAttention(nn.Module):
 
-    def __init__(self, num_heads, head_size):
+    def __init__(self, num_heads, head_size, dropout):
         super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(head_size * num_heads, head_size) #head_size * num_heads = embed_dim
+        self.dropout = nn.Dropout(dropout)
+    
+    def forward(self, seq1, seq2):
+        out = torch.cat([head(seq1, seq2) for head in self.heads], dim = -1) #Finds all outputs for cross attention and turns them into a single vector
+        #This vector represents all of the changes a vector embedding will take
+
+        out = self.dropout(self.proj(out)) #Put it through a fully connected layer and apply dropout to it
+        return out
+
 
 class FeedForward(nn.Module):
 
-    def __init__(self, embed_dim, neuron_num, dropout):
+    def __init__(self, embed_dim, dropout):
         
         super().__init__()
 
         #w_up (neuron_num * embed_dim)
         #w_down (embed_dim * neuron_num)
+        #In this scenario, we assume neuron_num = embed_dim * 4
 
         self.model = nn.Sequential(
-            nn.Linear(embed_dim, neuron_num),
+            nn.Linear(embed_dim, embed_dim * 4),
             nn.ReLU(),
-            nn.Linear(neuron_num, embed_dim),
+            nn.Linear(embed_dim * 4, embed_dim),
             nn.Dropout(dropout)
         )
     
@@ -77,7 +89,13 @@ class Block(nn.Module):
 
     def __init__(self, embed_dim, n_head):
 
+        #embed_dim = embedding dimension, n_head = number of heads
+        #head_size = embedding dimensions per individual heads
+
         super().__init__()
+
+
+        head_size = embed_dim // n_head
 
 class Translator(nn.Module):
 
